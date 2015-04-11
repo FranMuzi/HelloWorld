ffead3d0bb7d97ec0357c02d36e47bf144fb26c5
FranMuzi <frammuzi5@gmail.com>
2015-04-11 11:37:41 +0200

    Aggiunto nome e giorno;

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld!");
+		System.out.println("Ciao Mondo da Francesco Muzi - 11.4.15");
 
 	}
 
