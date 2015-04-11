d52ce54be00c5b62c5e83c20144c3e897d2249f2
FranMuzi <frammuzi5@gmail.com>
2015-04-11 11:45:45 +0200

    Aggiunti nome e giorno;

---
+++ CiaoMondo/src/CiaoMondo.java
@@ -0,0 +1,10 @@
+
+public class CiaoMondo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ciao Mondo da Francesco Muzi - 11.4.15");
+
+	}
+
+}
